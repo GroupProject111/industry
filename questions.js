eb6a7e4ed8bdcfb60e8d1fae46a6dc0bb4005dff
GroupProject111 <100701542+GroupProject111@users.noreply.github.com>
2022-04-20 14:10:55 +0100

    quiz questions

---
+++ questions.js
@@ -0,0 +1,70 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does the “https://” at the beginning of a URL indicate, as opposed to ”http://” (without the “s”)?",
+    answer: "That the information entered into the site is encrypted",
+    options: [
+      "That the site has special high definiton",
+      "That the information entered into the site is encrypted",
+      "That the site is not accesible to certain computers",
+      "Not Sure"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is an example of a ”phishing attack” ?",
+    answer: "All of the above",
+    options: [
+      "Sending someone an email that contains a malicious link that is disguised to look like an email from someone the person knows",
+      "Creating a fake website that looks nearly identical to a real website in order to trick users into entering their login information", 
+      "Sending someone a text message that contains a malicious link that is disguised to look like a notification that the person has won a contest",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following four passwords is the most secure?",
+    answer: "Wgd!7i",
+    options: [
+      "House123",
+      "Wgd!7i",
+      "into*38",
+      "123456"
+    ]
+  },
+    {
+    numb: 4,
+    question: "”Private browsing” is a feature in many internet browsers that lets users access web pages without any information (like browsing history) being stored by the browser. Can internet service providers see the online activities of their subscribers when those subscribers are using private browsing?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      "Not sure",
+      ""
+    ]
+  },
+    {
+    numb: 5,
+    question: "Turning off the GPS function of your smartphone prevents any tracking of your phone's location",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "Not sure",
+      ""
+    ]
+  },
+      {
+    numb: 6,
+    question: "If a public Wi-Fi network (such as in an airport or café) requires a password to access, is it generally safe to use that network for sensitive activities such as online banking?",
+    answer: "No, it's not safe",
+    options: [
+      "Yes, it is safe",
+      "No, it's not safe",
+      "Not sure",
+      ""
+    ]
+  },
+
+];
